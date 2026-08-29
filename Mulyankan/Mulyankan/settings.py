92b7987f91e9661147095d6db034b80c06a47b3d
S Devipriya <sdevipriya1999@gmail.com>
2026-08-29 19:28:55 +0530

    Restructuring to move all non-django files to root 	renamed:    Mulyankan/.env.example -> .env.example 	modified:   Mulyankan/Mulyankan/settings.py

--- Mulyankan/Mulyankan/settings.py
+++ Mulyankan/Mulyankan/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Load environment variables from the root .env file
 load_dotenv(BASE_DIR / '.env')
